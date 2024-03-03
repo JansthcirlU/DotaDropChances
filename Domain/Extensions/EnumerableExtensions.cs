ab40f44b014e60296168586ca79c51e172dd515f
Jan Ulrichts <jan.ulrichts@gmail.com>
2024-03-03 09:32:08 +0100

    Added decimal valued extension methods for count to count beyond int.MaxValue.

--- Domain/Extensions/EnumerableExtensions.cs
+++ Domain/Extensions/EnumerableExtensions.cs
@@ -9,4 +9,27 @@ public static class EnumerableExtensions
             yield return item;
         }
     }
+
+    public static decimal CountDecimal<T>(this IEnumerable<T> source)
+    {
+        decimal count = 0;
+        foreach (var item in source)
+        {
+            count++;
+        }
+        return count;
+    }
+
+    public static decimal CountDecimal<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+    {
+        decimal count = 0;
+        foreach (var item in source)
+        {
+            if (predicate(item))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
 }
